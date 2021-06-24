98b65805eb3013e9f7b42c03c2288159d5f3e9b2
sohail <sohailqureshi82@gmail.com>
2021-06-24 18:06:57 +0530

    user.name Add: Welcome message

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -6,7 +6,7 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data Structure Problem statements");
+            Console.WriteLine("Data Structure Problem Statements");
         }
     }
 }
